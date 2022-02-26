d4260c8af3dfad58c2b275ac1e0cb415bfbd1bb0
H <hrvojep@yahoo.com>
2022-02-26 13:44:31 +1100

    force apply 13:44

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "diamond_dogs_url" {
   value = "http://${aws_eip.diamond_dogs.public_dns}"
 }
 
-output "diamond_dogs_ip7" {
+output "diamond_dogs_ip8" {
   value = "http://${aws_eip.diamond_dogs.public_ip}"
 }
